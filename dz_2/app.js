ba49a74119598a3462dabb862176389e83c8d8c1
OrestSochinskiy <sochinskiyorest@gmail.com>
2021-08-22 15:29:08 +0300

    DONE

--- dz_2/app.js
+++ dz_2/app.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const expressHbs = require('express-handlebars')
 const path = require('path')
+
+const {PORT} = require('./config/variables')
+
+const app = express()
+
+app.listen(PORT,()=>{
+    console.log("APP LISTEN",PORT);
+})
